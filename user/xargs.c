fa5bc8fe981be79f0ce66c33727a6fd4bb536d6f
Lo Chi Thanh <lochithanh160905@gmail.com>
2026-03-19 18:31:12 +0700

    handcode1

--- user/xargs.c
+++ user/xargs.c
@@ -1,84 +1,48 @@
-#include "kernel/types.h"
-#include "kernel/param.h"
-#include "user/user.h"
 
-#define MAXLINE 512
+
 
 int main(int argc, char* argv[]){
-	char* cmd[MAXARG];
-	char buf[MAXLINE];
-	int n = 1;
-	int cmd_argc = 0;
+	
+	char* new_argv[100];
+	
+	// copy to new command 
+	for(int i = 1; i < argc; i ++){
+		new_argv[i - 1] = argv[i];	
 
-	int arg_start = 1;
-
-	// xu ly option -n
-	if(argc > 2 && strcmp(argv[1], "-n") == 0){
-		n = atoi(argv[2]);
-		arg_start = 3;
 	}
 
 	
-
-	for(int i = arg_start; i < argc; i ++){
-		cmd[cmd_argc++] = argv[i];
-	}
-
 	char c;
-	int idx = 0; 
-	int collected = 0;
+	int id = 0;
+	char* buff; // chua 1 word
+	int c_word = 0;
 
+	while(read(0, &c, 1) == 1){
+		if(c == ' ' || c == '\n'){
+			buff[id] = '\0'; // ket thuc chuoi
+			new_argv[argc + c_word] = buff;
+			id = 0; // reset 
+			 
+			
+
+		}
+		else{
+				
+		}
+	)
 
 	
-	// doc 1 byte
-	while(read(0, &c, 1) == 1){
-		if(c == ' ' || c == "\n"){
-			if(idx == 0) continue;
-		
-		buf[idx] = '\0';
-		
-		cmd[cmd_argc + collected] = buf;
-
-		idx = 0;
-
-		if(collected == n || c = '\n'){
-			cmd[cmd_argc + collected] = 0;
 
 
-			int pid = fork();
-			if(pid == 0){
-				exec(cmd[0], cmd);
-				fprintf(2, "exec failed \n");
-			} else {
-				wait(0);
-			}
 
-			collected = 0;
 
-		}
 
-		}
-	else {
-		buf[idx++] = c;
-	}
-	}
-	// xu ly input cuoi neu ko co newline
-	if(idx > 0){
-		buf[idx] = '\0';
 
-		cmd[cmd_argc + collected] = buf;
-		collected ++;
 
-		cmd[cmd_argc + collected] = 0;
 
-		int pid = fork();
-		if(pid == 0){
-			exec(cmd[0], cmd);
-			exit(1);
-		}
-		else {
-			wait(0);
-		}
-	}
-	exit(0);
+
+
 }
+
+
+
