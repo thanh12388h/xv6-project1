d75df56a7b1d5ac2484a6e8e74ccdbd2e69f1cff
Lo Chi Thanh <lochithanh160905@gmail.com>
2026-03-21 01:58:24 +0700

    completed feature_1

--- user/xargs.c
+++ user/xargs.c
@@ -1,8 +1,11 @@
 #include "kernel/types.h"
 #include "user/user.h"
+#include "kernel/param.h"
+
+#define MAXLINE 512
 
 int main(int argc, char* argv[]){
-	char* new_argv[100];
+	char* new_argv[MAXARG];
 	int new_argc = 0;
 	
 	// copy to new command 
@@ -10,37 +13,83 @@ int main(int argc, char* argv[]){
 		new_argv[new_argc++] = argv[i];	
 	}
 
-	
 	char c;
 	int id = 0;
-	char buff[100]; // chua 1 word
+	char buff[MAXLINE]; // chua 1 word
 	int c_word = 0;
 	
 	// gia su chac chan co du lieu tu stdin
 	// logic doc tung dong du lieu 
 	while(read(0, &c, 1) == 1){
-		if(c == '\n'){
-			buff[id] = '\0'; // ket thuc chuoi
-			new_argv[new_argc + c_word] = buff;
-			id = 0; // reset 
-			c_word++; // them 1 argument moi
+		// neu doc duoc 1 word hoac xuong dong thi dung lai xu ly
+		if(c == '\n' || c == ' '){
+			// xoa khoang trang thua, chi nap word ("hello" not "   hello" or "hello  ")
+			if(id == 0) continue;
 			
-			// logic thuc thi command dau tien (echo + new_cmd)
+			
+			buff[id] = '\0'; // ket thuc chuoi
+			
+			char* word = malloc(strlen(buff) + 1);
+			strcpy(word, buff);
+	
+			new_argv[new_argc + c_word++] = word; //*
+			
+			
+			id = 0; // reset 
+			
+			
+			// neu da het 1 dong, exec
+			if(c == '\n'){
 				new_argv[new_argc + c_word] = 0; // tu cuoi luon la NULL
+				
 				int p_id = fork();
 				if(p_id == 0){ // child process
 					exec(new_argv[0], new_argv);// echo + new cmd
 					printf("exec fail \n");
+					exit(1);
 				}
 				else{ // parent process 
 					wait(0);
+					for(int i = 0; i < c_word; i ++){
+						free(new_argv[new_argc + i]); // giai phong cac word da dung xong
+						new_argv[new_argc + i] = 0; // dat lai cac word trong la NULL de bat dau doc dong tiep theo
+					}
+					c_word = 0; // reset doc dong moi 
 				}
-			
+			}
 		}
 		else{
 			// day ky tu vao buff (word)
 			buff[id++] = c;
 		}
+	}
+	
+	// handle EOF: neu dong cuoi cua stdin khong co newline \n
+	if(id > 0){
+		buff[id] = '\0'; // ket thuc chuoi
+		
+		char* word = malloc(strlen(buff) + 1);
+		strcpy(word, buff);
+
+		new_argv[new_argc + c_word++] = word; //*
+		
+		
+		new_argv[new_argc + c_word] = 0; 
+		
+		int p_id = fork();
+		if(p_id == 0){ 
+			exec(new_argv[0], new_argv);
+			printf("exec fail \n");
+			exit(1);
+		}
+		else{ 
+			wait(0);
+			for(int i = 0; i < c_word; i ++){
+				free(new_argv[new_argc + i]);  
+				new_argv[new_argc + i] = 0; 
+			}
+		}
+	
 	}
 
 
