8ac4ff1e455fe243ef8b5a6c170355bd93e98acb
TranPhuocTai <trantai3526@gmail.com>
2026-03-22 22:01:40 +0700

    add tracing

--- kernel/proc.c
+++ kernel/proc.c
@@ -299,6 +299,8 @@ fork(void)
   // copy saved user registers.
   *(np->trapframe) = *(p->trapframe);
 
+  np->tracemask = p->tracemask;
+
   // Cause fork to return 0 in the child.
   np->trapframe->a0 = 0;
 
